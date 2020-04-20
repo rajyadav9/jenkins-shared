888749bda4f10bc7e20b83a2b8d270f01a246973
Raj Yadav <raj.yadav@sequoia.com>
2020-04-20 17:56:44 +0530

    jdsnvnc

---
+++ vars/abc.groovy
@@ -0,0 +1,13 @@
+package net.my.jenkins.workflow
+import com.cloudbees.groovy.cps.NonCPS
+def call(Map stageParams)
+{
+    sh"""
+echo "abcccccc"
+echo "${env.PROJECT_NAME}" 
+echo stageParams.REGION
+
+
+
+"""
+}
