e4d712c0e72b73e17f736d3567b51646d9615910
Raj Yadav <raj.yadav@sequoia.com>
2020-04-24 15:29:46 +0530

    jbjsb

--- vars/abort.groovy
+++ vars/abort.groovy
@@ -1,15 +1,14 @@
 package net.my.jenkins.workflow
 import com.cloudbees.groovy.cps.NonCPS
-def call(Map params){
+def call(Map params) {
 
-    if(currentBuild.getPreviousBuildInProgress()!= null)
-    {
+    if (currentBuild.getPreviousBuildInProgress() != null) {
 
-        env.aa = currentBuild.getPreviousBuildInProgress().getRawBuild().actions.find{ it instanceof ParametersAction }?.parameters.find{it.name == 'tag'}?.value
+        env.aa = currentBuild.getPreviousBuildInProgress().getRawBuild().actions.find { it instanceof ParametersAction }?.parameters.find { it.name == 'tag' }?.value
         echo "${env.aa}"
-        if(params.tag == env.aa)
-        {
-            milestone label: '', ordinal:  Integer.parseInt(env.BUILD_ID) - 1
-            milestone label: '', ordinal:  Integer.parseInt(env.BUILD_ID)
+        if (params.tag == env.aa) {
+            milestone label: '', ordinal: Integer.parseInt(env.BUILD_ID) - 1
+            milestone label: '', ordinal: Integer.parseInt(env.BUILD_ID)
         }
     }
+}
